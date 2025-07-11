2d5ed5ca60cff6c13fae1f88d8b3ebca36c0fe5c
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-11 09:00:59 +0900

    first commit

---
+++ src/test/java/eureca/capstone/project/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package eureca.capstone.project.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
