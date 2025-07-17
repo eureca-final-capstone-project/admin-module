190db62f2226782c06c898ed5576649ef2e802ff
Jang-GO <jang8195@naver.com>
2025-07-17 14:37:56 +0900

    VT2-92 feat: 스웨거에 인증 적용

--- src/main/java/eureca/capstone/project/admin/auth/controller/AuthController.java
+++ src/main/java/eureca/capstone/project/admin/auth/controller/AuthController.java
@@ -7,6 +7,8 @@ import eureca.capstone.project.admin.auth.service.TokenService;
 import eureca.capstone.project.admin.common.constant.RedisConstant;
 import eureca.capstone.project.admin.common.dto.base.BaseResponseDto;
 import eureca.capstone.project.admin.common.service.RedisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증 관리", description = "관리자 로그인 및 로그아웃을 처리하는 API") // swagger
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -28,7 +31,7 @@ public class AuthController {
     private final TokenService tokenService;
     private final RedisService redisService;
 
-
+    @Operation(summary = "관리자 로그인", description = "이메일과 비밀번호로 로그인하여 Access Token을 발급받습니다.") // swagger
     @PostMapping("/login")
     public BaseResponseDto<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse httpServletResponse){
         Authentication authentication = authenticationManager.authenticate(
@@ -53,6 +56,7 @@ public class AuthController {
         return success;
     }
 
+    @Operation(summary = "관리자 로그아웃", description = "현재 로그인된 사용자를 로그아웃 처리합니다. 서버에 저장된 Refresh Token이 삭제됩니다. <br> **(주의: 요청 시 Authorization 헤더에 유효한 Access Token을 포함해야 합니다.)**") // swagger
     @PostMapping("/logout")
     public BaseResponseDto<Void> logout(@AuthenticationPrincipal CustomUserDetailsDto customUserDetailsDto) {
         // 요청 값 로그 출력
---
+++ src/main/java/eureca/capstone/project/admin/common/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package eureca.capstone.project.admin.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth"; // Security Scheme 이름 정의
+
+        return new OpenAPI()
+                .info(new Info().title("Todolist API")
+                        .description("Todolist Application API Documentation")
+                        .version("v1.0"))
+                // SecurityRequirement를 전역적으로 추가
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                // Components에 SecurityScheme 정의 추가
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
