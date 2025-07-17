3a9c8fb4e35b5ee5fbcfd30652d2b80cc228ccb5
Jang-GO <jang8195@naver.com>
2025-07-17 15:01:24 +0900

    VT2-92 chore: 화이트리스트 경로 및 스웨거 설명 변경

--- src/main/java/eureca/capstone/project/admin/auth/constant/FilterConstant.java
+++ src/main/java/eureca/capstone/project/admin/auth/constant/FilterConstant.java
@@ -2,6 +2,7 @@ package eureca.capstone.project.admin.auth.constant;
 
 public final class FilterConstant {
     public static final String[] whiteList = {
+            "/admin/auth/login",
             "/swagger-ui/**",
             "/swagger-ui.html",
             "/error",
--- src/main/java/eureca/capstone/project/admin/report/controller/ReportController.java
+++ src/main/java/eureca/capstone/project/admin/report/controller/ReportController.java
@@ -34,7 +34,8 @@ public class ReportController {
     @Operation(summary = "신고 내역 목록 조회", description = "신고 내역을 페이징하여 조회합니다. status 파라미터로 필터링할 수 있습니다.")
     @GetMapping("/reports/history")
     public BaseResponseDto<Page<ReportHistoryDto>> getReportHistoryList(
-            @Parameter(description = "필터링할 신고 상태 (예: PENDING, AI_ACCEPTED 등)") @RequestParam(required = false) String statusCode,
+            @Parameter(description = "필터링할 신고 상태 <br>(PENDING, AI_ACCEPTED, AI_REJECTED, ADMIN_ACCEPTED, ADMIN_REJECTED, COMPLETED, REJECTED)")
+            @RequestParam(required = false) String statusCode,
             Pageable pageable) {
         return BaseResponseDto.success(reportService.getReportHistoryListByStatusCode(statusCode, pageable));
     }
@@ -42,7 +43,7 @@ public class ReportController {
     @Operation(summary = "제재 내역 목록 조회", description = "제재 대상 내역을 페이징하여 조회합니다. status 파라미터로 필터링할 수 있습니다.")
     @GetMapping("/restrictions")
     public BaseResponseDto<Page<RestrictionDto>> getRestrictionList(
-            @Parameter(description = "필터링할 신고 상태 (예: PENDING, ACCEPTED 등)") @RequestParam(required = false) String statusCode,
+            @Parameter(description = "필터링할 제재 상태 (PENDING, COMPLETED, REJECTED)") @RequestParam(required = false) String statusCode,
             Pageable pageable) {
         return BaseResponseDto.success(reportService.getRestrictionListByStatusCode(statusCode, pageable));
     }
