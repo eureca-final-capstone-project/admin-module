7724a9b959df9d7be9b860cf8df9d901de08c01d
so-yeon1 <psy92411@gmail.com>
2025-07-29 17:23:45 +0900

    feat: 제재 로직 중간테이블 조회로 변경

--- src/main/java/eureca/capstone/project/admin/report/entity/RestrictionAuthority.java
+++ src/main/java/eureca/capstone/project/admin/report/entity/RestrictionAuthority.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "restriction_authority")
+@Table(name = "restriction_type_authority")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RestrictionAuthority {
